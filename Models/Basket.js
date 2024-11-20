32609c870caaeeb7ed6352aba142964021628ffe
AsadovIbrahim <ibrahimasadov31@gmail.com>
2024-11-20 21:25:05 +0400

    model uptaded

--- Models/Basket.js
+++ Models/Basket.js
@@ -1,13 +1,19 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
-const basketSchema=new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    products:[
-        {
-            productId:{type:mongoose.Schema.Types.ObjectId,ref:"Product",required:true},
-            quantity:{type:Number,required:true}
-        }
-    ],
-},{timestamps:true});
+const basketSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  products: [
+    {
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+      quantity: { type: Number, required: true }
+    }
+  ],
+  totalPrice: { type: Number, required: true },
+  currency: { 
+    type: String, 
+    enum: ["$", "€", "₼"], 
+    required: true 
+  }
+}, { timestamps: true });
 
-module.exports=mongoose.model('Basket',basketSchema);
+module.exports = mongoose.model('Basket', basketSchema);
